e3daacd1ea5919b2c8e9d4d12cd157c94c5282eb
Filippo <filippo.greppi2@studio.unibo.it>
2026-02-02 16:53:36 +0100

    refactor: simplify Message struct by removing unused fields and includes

--- assignment-03/tms/src/kernel/Message.hpp
+++ assignment-03/tms/src/kernel/Message.hpp
@@ -1,16 +1,12 @@
 #ifndef __MESSAGE__
 #define __MESSAGE__
 
-#include <Arduino.h>
+#define PAYLOAD_SIZE 32
 
-#define OP_LEVEL_UPDATE 0x01
-#define OP_SET_VALVE 0x02
-#define OP_SET_MODE 0x03
-
-struct Message
+typedef struct Message
 {
-    uint8_t opcode;
-    char payload[32];
-};
+    const char* topic;
+    char payload[PAYLOAD_SIZE];
+} Message;
 
 #endif  // __MESSAGE__
